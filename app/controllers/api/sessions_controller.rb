f22da87c243a737e793d4a5f75ddf61bab53fcbc
Ahmed <ahmed96razi39@gmail.com>
2023-06-14 09:29:56 -0700

    frontend phase 0 finished

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,8 +2,7 @@ class Api::SessionsController < ApplicationController
   def show
     if current_user
       @user = current_user
-      # render 'api/users/show'
-      
+      render 'api/users/show'
     else
       render json: { user: nil }
     end
@@ -14,17 +13,18 @@ class Api::SessionsController < ApplicationController
     password = params[:password]
     @user = User.find_by_credentials(username, password)
     if @user 
-        login!(@user)
-        # render "api/users/show"
-        render json: @user
+        login!(@user) # consistent with application controller login func
+        render "api/users/show"
     else 
         render json: {errors: ["The provided credentials were invalid."]}, status: 422
     end 
   end
 
   def destroy
-    logout
+    logout! # consistent with application controller logout func 
     head :no_content
   end
 
 end
+
+
---
+++ app/views/api/users/show.json.jbuilder
@@ -0,0 +1,7 @@
+
+json.user do
+  json.extract! @user, :id, :email, :username, :created_at, :updated_at
+end
+
+
+
