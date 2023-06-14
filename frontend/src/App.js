cb60461dae0462ed16f88e80ff882daa05960d77
Ahmed <ahmed96razi39@gmail.com>
2023-06-14 16:08:57 -0700

    done with SignUp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import SignupFormPage from "./components/SignupFormPage";
 
 function App() {
+  // return (
+  //   <Switch>
+  //     <Route path="/login">
+  //       <LoginFormPage />
+  //     </Route>
+  //   </Switch>
+  // );
   return (
     <Switch>
       <Route path="/login">
         <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
     </Switch>
   );
