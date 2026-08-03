c365caa7f59023fc6ccf124c225d520e34e13db5
Ahmed <ahmed96razi39@gmail.com>
2026-08-03 10:51:45 -0700

    boxes

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,10 +14,10 @@
 #
 class Product < ApplicationRecord
   validates :name, :description, :price, :category, presence: true
-  validates :price, numericality: { greater_than: 0 }
+  validates :price, numericality: { greater_than: 0 }, age
   validates :name, uniqueness: true
 
-  has_one_attached :photo
+  has_one_attached :photo;
 
   has_many :reviews,
   foreign_key: :product_id,
